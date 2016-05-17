6e3328befba04694011270d7787c126c23fbfd58
Gethin James <gethin.james@alfresco.com>
2016-05-17 10:19:48 +0200

    Updated the validate command

--- validate.sh
+++ validate.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
-docker run -it -v $(pwd):/app:rw --rm alfresco/swagger-tester:1.0 swagger validate /app/src/main/webapp/definitions/alfresco-core.yaml
-docker run -it -v $(pwd):/app:rw --rm alfresco/swagger-tester:1.0 swagger validate /app/src/main/webapp/definitions/alfresco-auth.yaml
-docker run -it -v $(pwd):/app:rw --rm alfresco/swagger-tester:1.0 swagger validate /app/src/main/webapp/definitions/alfresco-workflow.yaml
+docker run -v $(pwd):/app:rw alfresco/swagger-tester:1.0 swagger validate /app/src/main/webapp/definitions/alfresco-core.yaml
+docker run -v $(pwd):/app:rw alfresco/swagger-tester:1.0 swagger validate /app/src/main/webapp/definitions/alfresco-auth.yaml
+docker run -v $(pwd):/app:rw alfresco/swagger-tester:1.0 swagger validate /app/src/main/webapp/definitions/alfresco-workflow.yaml
