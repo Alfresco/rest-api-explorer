b450a94cb8a7ef3acf6a5da0f92645d8de7e2aa8
Gethin James <gethin.james@alfresco.com>
2016-05-24 16:59:17 +0200

    RA-1023: Updated the swagger-test version to match the swagger-cli version

--- validate.sh
+++ validate.sh
@@ -2,5 +2,5 @@
 set -e
 
 for filename in src/main/webapp/definitions/*.yaml; do
-      docker run -v $(pwd):/app:rw alfresco/swagger-tester:1.0 swagger validate "/app/$filename"
+      docker run -v $(pwd):/app:rw dockerreg.alfresco.com/swagger-tester:1.0.0-beta.2 swagger validate "/app/$filename"
 done
