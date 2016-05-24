7d5a50e4f9eceb44ce43c22e2c9a222b62271ce5
Gethin James <gethin.james@alfresco.com>
2016-05-24 16:54:43 +0200

    RA-1023: Added a validate.sh shell script

--- validate.sh
+++ validate.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
-docker run -v $(pwd):/app:rw alfresco/swagger-tester:1.0 swagger validate /app/src/main/webapp/definitions/alfresco-core.yaml
-docker run -v $(pwd):/app:rw alfresco/swagger-tester:1.0 swagger validate /app/src/main/webapp/definitions/alfresco-auth.yaml
-docker run -v $(pwd):/app:rw alfresco/swagger-tester:1.0 swagger validate /app/src/main/webapp/definitions/alfresco-workflow.yaml
+set -e
+
+for filename in src/main/webapp/definitions/*.yaml; do
+      docker run -v $(pwd):/app:rw alfresco/swagger-tester:1.0 swagger validate "/app/$filename"
+done
