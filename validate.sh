7fcc9fa25d6abca9f7d87fdc8cacd390c296b303
Gavin Cornwell <gavin.cornwell@alfresco.com>
2016-12-09 10:55:38 +0000

    REPO-1701: REST-API Explorer should clean the Docker container it creates

--- validate.sh
+++ validate.sh
@@ -1,6 +1,12 @@
 #! /bin/bash
 set -e
 
+echo "Validating OpenAPI specs..."
+
 for filename in src/main/webapp/definitions/*.yaml; do
       docker run -v $(pwd):/app:rw dockerreg.alfresco.com/swagger-tester:1.0.0-beta.2 swagger validate "/app/$filename"
 done
+
+echo
+echo "Removing exited docker images..."
+docker rm -v $(docker ps -a -q -f status=exited)
