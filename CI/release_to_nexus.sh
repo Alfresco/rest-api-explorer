411bc9de5353d20898229ca44be49a9c143fa358
Andrea Ligios <andrea.ligios@alfresco.com>
2021-01-21 19:15:50 +0100

    Build Tools

--- CI/release_to_nexus.sh
+++ CI/release_to_nexus.sh
@@ -7,6 +7,7 @@ readonly GITHUB_PASSWORD=$3
 echo "RELEASE VERSION: $RELEASE_VERSION"
 
 mvn release:clean release:prepare release:perform \
+      -B \
       -DskipTests \
       -Dbuild-number="${TRAVIS_BUILD_NUMBER}" \
       -DreleaseVersion="$RELEASE_VERSION" \
