bef49b1a9f747232e6cfb9c740dd0136771762a7
Andrea Ligios <andrea.ligios@alfresco.com>
2021-01-21 13:13:02 +0100

    Third Travis Pipeline Test

--- CI/release.sh
+++ CI/release.sh
@@ -3,5 +3,5 @@
 readonly RELEASE_VERSION=$1
 
 echo "RELEASE VERSION: $RELEASE_VERSION"
-
+echo "RELEASE VERSION2: $RELEASE_VERSION2"
 #mvn -DreleaseVersion=${release.version} -DignoreSnapshots -DdevelopmentVersion=${next.version} release:prepare release:perform
