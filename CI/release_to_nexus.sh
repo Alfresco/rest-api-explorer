2b3a0696123235d06fc82f96040b8982ed193053
Andrea Ligios <andrea.ligios@alfresco.com>
2021-01-22 09:26:00 +0100

    Release test 1

--- CI/release_to_nexus.sh
+++ CI/release_to_nexus.sh
@@ -6,6 +6,9 @@ readonly GITHUB_PASSWORD=$3
 
 echo "RELEASE VERSION: $RELEASE_VERSION"
 
+# Needed for testing it from the dev branch
+git checkout -f master
+
 mvn release:clean release:prepare release:perform \
       -B \
       -DskipTests \
