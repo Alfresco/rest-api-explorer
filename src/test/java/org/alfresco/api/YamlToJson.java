66b4d413e4e2015753a247d41fc8db9309c8b55c
dhrn <14145706+dhrn@users.noreply.github.com>
2021-02-05 15:24:50 +0530

    Aspect and Type api implementation (#119)
    * [REPO-5440] Aspect api implementation
    
    * * fixed comments
    
    * * minor improvements
    
    * * example fixed
    
    * minor update
    
    * * fix docs
    
    * * fixed
    
    * * fixed comments
    
    * * improved docs
    
    * * types added
    
    * * fixed comments

--- src/test/java/org/alfresco/api/YamlToJson.java
+++ src/test/java/org/alfresco/api/YamlToJson.java
@@ -56,6 +56,10 @@ public class YamlToJson {
     public static final String DISCOVERY_JSON_DEFINITION = JSON_DESTINATION + "alfresco-discovery.json";
     public static final String DISCOVERY_DEFINITION_TITLE = "Alfresco Discovery REST API";
 
+    public static final String MODEL_DEFINITION = "/src/main/webapp/definitions/alfresco-model.yaml";
+    public static final String MODEL_JSON_DEFINITION = JSON_DESTINATION + "alfresco-model.json";
+    public static final String MODEL_DEFINITION_TITLE = "Alfresco Model REST API";
+
     public static void main(String[] args) {
 
         String rootPath = args[0];
@@ -88,6 +92,10 @@ public class YamlToJson {
             //Discovery
             swagger = parseSwaggerDef(new File(rootPath + DISCOVERY_DEFINITION), DISCOVERY_DEFINITION_TITLE);
             Json.mapper().writeValue(new File(rootPath + DISCOVERY_JSON_DEFINITION), swagger);
+
+            //Model
+            swagger = parseSwaggerDef(new File(rootPath + MODEL_DEFINITION), MODEL_DEFINITION_TITLE);
+            Json.mapper().writeValue(new File(rootPath + MODEL_JSON_DEFINITION), swagger);
 
         } catch (IOException e) {
             System.err.println("Failed to create a json definitions: " + e.getLocalizedMessage());
