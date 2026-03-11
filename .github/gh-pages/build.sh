811626d340debe8bbb710e97ad729b4adeb0fe15
Giovanni Toraldo <71768+gionn@users.noreply.github.com>
2026-03-11 11:23:38 +0100

    OPSEXP-3904 Expose swagger under /api-explorer/ for retrocompat (#241)

--- .github/gh-pages/build.sh
+++ .github/gh-pages/build.sh
@@ -10,33 +10,39 @@ fi
 
 BUILD_DIR=$(dirname "$0")
 ARCHIVE_DEST_DIR="./build"
-DEFS_DEST_DIR="$ARCHIVE_DEST_DIR/dist"
-SWAGGER_INIT_DEST_DIR="$ARCHIVE_DEST_DIR/dist/swagger-initializer.js"
-
+SWAGGER_UI_DEST_DIR="$ARCHIVE_DEST_DIR/api-explorer"
+SWAGGER_INIT_JS_PATH="$SWAGGER_UI_DEST_DIR/swagger-initializer.js"
 SWAGGER_UI_DOWNLOAD_URL="https://github.com/swagger-api/swagger-ui/archive/refs/tags/v${SWAGGER_UI_VERSION}.tar.gz"
 
 cd "$BUILD_DIR"
 
 echo "Cleaning up previous build workspace at $ARCHIVE_DEST_DIR"
 rm -rf "$ARCHIVE_DEST_DIR"
-
 mkdir -p "$ARCHIVE_DEST_DIR"
-curl -sfL "$SWAGGER_UI_DOWNLOAD_URL" | tar -xz -C "$ARCHIVE_DEST_DIR" \
-  --strip-components=1 swagger-ui-$SWAGGER_UI_VERSION/dist/
 
-mkdir -p "$DEFS_DEST_DIR"
-cp ./../../src/main/webapp/definitions/*.yaml "$DEFS_DEST_DIR"/
-cp ./swagger-initializer.js "$SWAGGER_INIT_DEST_DIR"
+echo "Create index.html with redirect to api-explorer/ folder"
+cp ./redirect.html "$ARCHIVE_DEST_DIR/index.html"
 
-echo "Build completed at $ARCHIVE_DEST_DIR/dist"
-echo "Verifying that all definitions files are present in $SWAGGER_INIT_DEST_DIR"
+echo "Creating Swagger UI directory at $SWAGGER_UI_DEST_DIR"
+mkdir -p "$SWAGGER_UI_DEST_DIR"
 
-for file in "$DEFS_DEST_DIR"/*.yaml; do
+echo "Downloading Swagger UI version $SWAGGER_UI_VERSION and unpacking to $SWAGGER_UI_DEST_DIR"
+curl -sfL "$SWAGGER_UI_DOWNLOAD_URL" | tar -xz -C "$SWAGGER_UI_DEST_DIR" \
+  --strip-components=2 swagger-ui-$SWAGGER_UI_VERSION/dist/
+
+echo "Copying API definitions and initializer config to $SWAGGER_UI_DEST_DIR"
+cp ./../../src/main/webapp/definitions/*.yaml "$SWAGGER_UI_DEST_DIR"/
+cp ./swagger-initializer.js "$SWAGGER_INIT_JS_PATH"
+
+echo "Build completed at $SWAGGER_UI_DEST_DIR"
+echo "Verifying that all definitions files are present in $SWAGGER_INIT_JS_PATH"
+
+for file in "$SWAGGER_UI_DEST_DIR"/*.yaml; do
   filename=$(basename "$file")
-  if ! grep -Fq "$filename" "$SWAGGER_INIT_DEST_DIR"; then
-    echo "Error: $filename is not present in $SWAGGER_INIT_DEST_DIR"
+  if ! grep -Fq "$filename" "$SWAGGER_INIT_JS_PATH"; then
+    echo "Error: $filename is not present in $SWAGGER_INIT_JS_PATH"
     exit 1
   fi
 done
 
-echo "All definitions files are present in $SWAGGER_INIT_DEST_DIR"
+echo "All definitions files are present in $SWAGGER_INIT_JS_PATH"
